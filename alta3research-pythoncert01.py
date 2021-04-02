1c076aa114196638d53628b0c13250886280cea4
JamesAaronCaldwell <76846927+JamesAaronCaldwell@users.noreply.github.com>
2021-04-02 13:00:14 -0500

    Create alta3research-pythoncert01.py

---
+++ alta3research-pythoncert01.py
@@ -0,0 +1,11 @@
+#! /usr/bin/python3
+"""This is a script by James Aaron Caldwell to demonstrate basic understanding of python scripting language"""
+
+#Import examples using standard library 
+
+#Make an API request to https://api.nasa.gov and display some portion of the data returned in an easily read format
+
+#Use matplotlib to produce a graph
+#Write a guessing game that uses the crayons library to post in green YOU WIN when the correct answer is supplied
+#Use pandas to create a dataframe, and export some data into a format of your choice
+#Anything else you find interesting!
