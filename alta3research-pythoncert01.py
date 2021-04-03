8f39507c3843020fd91d8a89677c1e240a726dda
JamesAaronCaldwell <76846927+JamesAaronCaldwell@users.noreply.github.com>
2021-04-03 00:08:25 -0500

    Completed Guessing game / Nasa 50% / Graph 0%

--- alta3research-pythoncert01.py
+++ alta3research-pythoncert01.py
@@ -1,79 +1,71 @@
 #! /usr/bin/python3
 """This is a script by James Aaron Caldwell to demonstrate basic understanding of python scripting language"""
 
-
-#Import examples using standard library 
+# Import examples using standard library
 import requests
+import json
+import pandas as pd
+import matplotlib
+import crayons
+import time
+
+# global
+API = "http://www.neowsapp.com/rest/v1/neo/2216523?api_key=7cqHW801kCstlBsbcprRwwe01awpGk7XVVamgD0d"  # API with my key
 
 
-#globals
-API = "https://api.nasa.gov/planetary/apod?api_key=7cqHW801kCstlBsbcprRwwe01awpGk7XVVamgD0d" #API with my key
-NUMTODL = 10 # max number of pictures to DL
-
-
-#Make func to Nasa API request to https://api.nasa.gov and display some portion of the data returned in an easily read format
+# Make func to Nasa API request to https://api.nasa.gov and 
+#    * Use pandas to create a dataframe, and export some data into a format of your choice
+#    display some portion of the data returned in an easily read format
 def nasa():
+    #counter = 0
+    r = requests.get(API)
+    # Kills app if API is down
+    if r.status_code != 200:
+        print("Error with API connection, try again later.")
+        exit()
     
+    print(json.dumps(r.json(), indent = 1)) #print formatted json 
 
 
-    #Use pandas to create a dataframe, and export some data into a format of your choice
-    
-    #Use matplotlib to produce a graph
+#Use matplotlib for graphed data
+def graph():
+    print("Graph!")
 
-    #Anything else you find interesting!
-
-
-    
-
-            counter = 0
-        r = requests.get(API)
-
-        # we want to grab the data off the 200 response
-        nasadata = r.json()
-
-        # loop across all the dictionaries within the list called "photos"
-        for pic in nasadata.get("photos"):
-           
-            # increase the counter by 1
-            counter += 1
-            # we want to snag the value mapped to photos."pic".img_src
-            # print(pic.get("img_src"))
-            linktodownload = pic.get("img_src")
-
-            # download the image
-            imgdl = requests.get(linktodownload)
-            # grab name of image
-            imgNm = linktodownload.split("/")[-1]
-
-            # show which image is about to be downloaded
-            print(f"Downloading Image #{counter}: {imgNm}")
-
-            # open the file we want to write our image to
-            with open(f"/home/student/static/{imgNm}", 'wb') as f:
-                # write the image
-                f.write(imgdl.content)
-        
-            # if counter reaches the max number of photos to download
-            # exit the loop (break)
-            if counter == NUMTODL:
-                break
+#    * Write a guessing game that uses the crayons library 
+#   to post in green YOU WIN when the correct answer is supplied
+def guess():
+    round = 0
+    answer = " "
+    while round < 3 and answer != "Purple":
+        round += 1     # increase the round counter by 1
+        answer = input(crayons.blue('What is your favorite color? : '))
+        if answer == "Purple": # logic to check if user gave correct answer
+            print(crayons.green("You Win!!!"))
+        elif round == 3:    # logic to ensure round has not yet reached 3
+            print(crayons.yellow("Sorry, the answer was Purple."))
+        else:                 # if answer was wrong
+            print(crayons.yellow("Sorry. Try again!"))
 
 
-
+def banner(words):
+    print(crayons.blue("******************************************************"))
+    print(crayons.blue("******************************************************"))
+    print(crayons.red("             "+words +"                         "))
+    print(crayons.blue("******************************************************"))
+    print(crayons.blue("******************************************************"))
+    time.sleep(1)
 # Main will ask for user input to choose what app will run
-# One choice is Nasa API 
+# One choice is Nasa API
 # One choice is Color example
 
 def main():
-
+    banner("Starting NASA API Call...")
+    nasa()
+    banner("Graphing Example...")
+    graph()
+    banner("Guessing Game Example...")
+    guess()
+    banner("James Aaron Caldwell Citigroup...")
 
 if __name__ == "__main__":
     main()
-
-
-
-
-#Write a guessing game that uses the crayons library to post in green YOU WIN when the correct answer is supplied
-
-
-
